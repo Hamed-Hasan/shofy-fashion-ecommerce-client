4f45afc63a992891397ae5767e9acf45f6f06c85
John Doe <hamedhasan422421@gmail.com>
2023-07-28 13:53:05 +0300

    Add Plus component SVG icon

---
+++ src/svg/plus.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Plus = () => {
+  return (
+    <svg
+      width="11"
+      height="12"
+      viewBox="0 0 11 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 6H10"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.5 10.5V1.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Plus;
