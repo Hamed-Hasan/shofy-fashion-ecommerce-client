46653a9c1058af071b573e814576450b0971550f
John Doe <hamedhasan422421@gmail.com>
2023-07-28 13:08:19 +0300

    - Create ArrowRightSm SVG component for displaying a small arrow pointing right. - The SVG includes a single path forming the arrow, with dimensions 7x12.

---
+++ src/svg/arrow-right-sm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ArrowRightSm = () => {
+  return (
+    <svg
+      width="7"
+      height="12"
+      viewBox="0 0 7 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1L6.02116 5.99958L1 11"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowRightSm;
