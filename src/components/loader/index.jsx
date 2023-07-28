1cab453461734daecfb776d54ecd40b33b5a06a9
John Doe <hamedhasan422421@gmail.com>
2023-07-28 21:43:35 +0300

    Add home loaders for category, gadgets, new arrivals, offers, products, and small products in the first section. Include home-2 loaders for category, featured products, popular products, all products, new products, and best-selling products in the second section. Introduce home-3 loaders for beauty category, products, two types of products, and trending products in the third section.

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,35 @@
+import HomeCateLoader from "./home/home-cate-loader";
+import HomeGadgetPrdLoader from "./home/home-gadget-prd-loader";
+import HomeNewArrivalPrdLoader from "./home/home-newArrival-prd-loader";
+import HomeOfferPrdLoader from "./home/home-offer-prd-loader";
+import HomePrdLoader from "./home/home-prd-loader";
+import HomeSmPrdLoader from "./home/home-sm-prd-loader";
+import HomeTwoCateLoader from "./home-2/home-2-cate-loader";
+import HomeTwoFeaturedPrdLoader from "./home-2/home-2-featured-loader";
+import HomeTwoPopularPrdLoader from "./home-2/home-2-popular-loader";
+import HomeTwoPrdLoader from "./home-2/home-2-prd-loder";
+import HomeTwoNewPrdPrdLoader from "./home-2/home-2-newPrd-loader";
+import HomeTwoBestSellPrdPrdLoader from "./home-2/home-2-bestSell-loader";
+import HomeThreeCategoryLoader from "./home-3/beauty-category-loader";
+import HomeThreePrdLoader from "./home-3/beauty-prd-loader";
+import HomeThreeTrendingPrdLoader from "./home-3/beauty-trendingPrd-loader";
+import HomeThreePrdTwoLoader from "./home-3/beauty-prd-2-loader";
+
+export {
+  HomeCateLoader,
+  HomeGadgetPrdLoader,
+  HomeNewArrivalPrdLoader,
+  HomeOfferPrdLoader,
+  HomePrdLoader,
+  HomeSmPrdLoader,
+  HomeTwoCateLoader,
+  HomeTwoFeaturedPrdLoader,
+  HomeTwoPopularPrdLoader,
+  HomeTwoPrdLoader,
+  HomeTwoNewPrdPrdLoader,
+  HomeTwoBestSellPrdPrdLoader,
+  HomeThreeCategoryLoader,
+  HomeThreePrdLoader,
+  HomeThreePrdTwoLoader,
+  HomeThreeTrendingPrdLoader,
+}
