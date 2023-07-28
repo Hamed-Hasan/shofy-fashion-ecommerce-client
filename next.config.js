b269cfd388626a547f7ba1eb4b32ae1c5aef1f2d
John Doe <hamedhasan422421@gmail.com>
2023-07-28 11:42:12 +0300

    initial setup shofy

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com'],
+  },
+}
+
+module.exports = nextConfig
