7b2e91601c82dca293d1b6da529b2609e0a791de
John Doe <hamedhasan422421@gmail.com>
2023-07-28 15:07:15 +0300

    - Create FooterTwo component with different style variations - Include logo, contact information, and social media links in FooterTwo - Add My Account, Information, and Subscription sections with relevant links and content - Implement email subscription form in the Subscription section - Improve UI layout and styling for FooterTwo - Add copyright notice and payment methods section in FooterTwo

---
+++ src/layout/footers/footer-2.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+// internal
+import social_data from '@/data/social-data';
+import { Email, Location } from '@/svg';
+import logo from '@assets/img/logo/logo.svg';
+import pay from '@assets/img/footer/footer-pay.png';
+
+const FooterTwo = () => {
+  return (
+    <>
+      <footer>
+        <div className="tp-footer-area tp-footer-style-2 tp-footer-style-3 tp-footer-style-4" data-bg-color="#F5F5F5" style={{ backgroundColor: `rgb(245, 245, 245)` }}>
+          <div className="tp-footer-top pt-95 pb-40">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                  <div className="tp-footer-widget footer-col-4-1 mb-50">
+                    <div className="tp-footer-logo">
+                      <Link href="/">
+                        <Image src={logo} alt="logo" />
+                      </Link>
+                    </div>
+                    <div className="tp-footer-widget-content">
+                      <div className="tp-footer-talk mb-20">
+                        <span>Got Questions? Call us</span>
+                        <h4><a href="tel:670-413-90-762">+670 413 90 762</a></h4>
+                      </div>
+                      <div className="tp-footer-contact">
+                        <div className="tp-footer-contact-item d-flex align-items-start">
+                          <div className="tp-footer-contact-icon">
+                            <span>
+                              <Email />
+                            </span>
+                          </div>
+                          <div className="tp-footer-contact-content">
+                            <p><a href="mailto:shofy@support.com">shofy@mail.com</a></p>
+                          </div>
+                        </div>
+                        <div className="tp-footer-contact-item d-flex align-items-start">
+                          <div className="tp-footer-contact-icon">
+                            <span>
+                              <Location />
+                            </span>
+                          </div>
+                          <div className="tp-footer-contact-content">
+                            <p><a href="https://www.google.com/maps/place/Sleepy+Hollow+Rd,+Gouverneur,+NY+13642,+USA/@44.3304966,-75.4552367,17z/data=!3m1!4b1!4m6!3m5!1s0x4cccddac8972c5eb:0x56286024afff537a!8m2!3d44.3304928!4d-75.453048!16s%2Fg%2F1tdsjdj4" target="_blank">79 Sleepy Hollow St. <br /> Jamaica, New York 1432</a></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                  <div className="tp-footer-widget footer-col-4-2 mb-50">
+                    <h4 className="tp-footer-widget-title">My Account</h4>
+                    <div className="tp-footer-widget-content">
+                      <ul>
+                        <li><a href="#">Track Orders</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                        <li><a href="#">My Account</a></li>
+                        <li><a href="#">Order History</a></li>
+                        <li><a href="#">Returns</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-2 col-md-4 col-sm-5">
+                  <div className="tp-footer-widget footer-col-4-3 mb-50">
+                    <h4 className="tp-footer-widget-title">Infomation</h4>
+                    <div className="tp-footer-widget-content">
+                      <ul>
+                        <li><a href="#">Our Story</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Latest News</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-7">
+                  <div className="tp-footer-widget footer-col-4-4 mb-50">
+                    <h4 className="tp-footer-widget-title">Subcribe.</h4>
+                    <div className="tp-footer-widget-content">
+                      <div className="tp-footer-subscribe">
+                        <p>Our conversation is just getting started</p>
+                        <div className="tp-footer-subscribe-form mb-30">
+                          <form action="#">
+                            <div className="tp-footer-subscribe-input">
+                              <input type="email" placeholder="Enter Your Email" />
+                              <button type="submit">Subscribe</button>
+                            </div>
+                          </form>
+                        </div>
+                        <div className="tp-footer-social-4 tp-footer-social">
+                          <h4 className="tp-footer-social-title-4">Follow Us On</h4>
+                          {social_data.map(s => <a href={s.link} key={s.id} target="_blank">
+                            <i className={s.icon}></i>
+                          </a>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tp-footer-bottom">
+            <div className="container">
+              <div className="tp-footer-bottom-wrapper">
+                <div className="row align-items-center">
+                  <div className="col-md-6">
+                    <div className="tp-footer-copyright">
+                      <p>© {new Date().getFullYear()} All Rights Reserved  |  Next js Template by
+                        <Link href="/">ThemePure</Link>.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="tp-footer-payment text-md-end">
+                      <p>
+                        <Image src={pay} alt="pay" />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default FooterTwo;
