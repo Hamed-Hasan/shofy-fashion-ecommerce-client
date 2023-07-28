21576e7141261bf6f044b282937cd01afccb7458
John Doe <hamedhasan422421@gmail.com>
2023-07-28 12:03:17 +0300

    - Create ArrowNextTwo SVG component for displaying an icon representing a forward arrow.

---
+++ src/svg/arrow-next-2.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ArrowNextTwo = () => {
+  return (
+    <svg
+      width="16"
+      height="14"
+      viewBox="0 0 16 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.9993 6.99989L1 6.99989"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M9.35352 1.5L14.9999 6.99954L9.35352 12.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default ArrowNextTwo;
