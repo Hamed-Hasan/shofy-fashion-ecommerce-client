cd6dd93b95ef9042dec84a77ebe50a2c984a7b30
John Doe <hamedhasan422421@gmail.com>
2023-07-28 22:12:17 +0300

    Add ContactForm component with form validation Integrate react-hook-form for form handling Refactor error handling for form validation Implement contact form with name, email, subject, and message fields Add 'Save my name, email, and website' checkbox Notify user with success message after message submission Optimize code for form reset after submission

---
+++ src/components/forms/contact-form.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+// internal
+import { CloseEye, OpenEye } from "@/svg";
+import ErrorMsg from "../common/error-msg";
+import { notifyError, notifySuccess } from "@/utils/toast";
+
+// schema
+const schema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  subject: Yup.string().required().label("Subject"),
+  message: Yup.string().required().label("Subject"),
+  remember: Yup.bool()
+    .oneOf([true], "You must agree to the terms and conditions to proceed.")
+    .label("Terms and Conditions"),
+});
+
+const ContactForm = () => {
+
+    // react hook form
+    const {register,handleSubmit,formState: { errors },reset} = useForm({
+      resolver: yupResolver(schema),
+    });
+    // on submit
+    const onSubmit = (data) => {
+      if(data){
+        notifySuccess('Message sent successfully!');
+      }
+
+      reset();
+    };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} id="contact-form">
+      <div className="tp-contact-input-wrapper">
+        <div className="tp-contact-input-box">
+          <div className="tp-contact-input">
+            <input {...register("name", { required: `Name is required!` })} name="name" id="name" type="text" placeholder="Shahnewaz Sakil" />
+          </div>
+          <div className="tp-contact-input-title">
+            <label htmlFor="name">Your Name</label>
+          </div>
+          <ErrorMsg msg={errors.name?.message} />
+        </div>
+        <div className="tp-contact-input-box">
+          <div className="tp-contact-input">
+            <input {...register("email", { required: `Email is required!` })} name="email" id="email" type="email" placeholder="shofy@mail.com" />
+          </div>
+          <div className="tp-contact-input-title">
+            <label htmlFor="email">Your Email</label>
+          </div>
+          <ErrorMsg msg={errors.email?.message} />
+        </div>
+        <div className="tp-contact-input-box">
+          <div className="tp-contact-input">
+            <input {...register("subject", { required: `Subject is required!` })} name="subject" id="subject" type="text" placeholder="Write your subject" />
+          </div>
+          <div className="tp-contact-input-title">
+            <label htmlFor="subject">Subject</label>
+          </div>
+          <ErrorMsg msg={errors.subject?.message} />
+        </div>
+        <div className="tp-contact-input-box">
+          <div className="tp-contact-input">
+            <textarea {...register("message", { required: `Message is required!` })} id="message" name="message" placeholder="Write your message here..."/>
+          </div>
+          <div className="tp-contact-input-title">
+            <label htmlFor="message">Your Message</label>
+          </div>
+          <ErrorMsg msg={errors.message?.message} />
+        </div>
+      </div>
+      <div className="tp-contact-suggetions mb-20">
+        <div className="tp-contact-remeber">
+          <input  {...register("remember", {required: `Terms and Conditions is required!`})} name="remember" id="remember" type="checkbox" />
+          <label htmlFor="remember">Save my name, email, and website in this browser for the next time I comment.</label>
+          <ErrorMsg msg={errors.remember?.message} />
+        </div>
+      </div>
+      <div className="tp-contact-btn">
+        <button type="submit">Send Message</button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
