ff4ffbcedc2acef44321b4870305bcb8ab9c0657
John Doe <hamedhasan422421@gmail.com>
2023-07-28 17:13:03 +0300

    - Create ProductDetailsPageWithVideo component to display product details with video - Fetch product data using useGetProductQuery hook from Redux store - Display product details and breadcrumb on the page

---
+++ src/pages/product-details-video/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+// internal
+import SEO from '@/components/seo';
+import HeaderTwo from '@/layout/headers/header-2';
+import Footer from '@/layout/footers/footer';
+import Wrapper from '@/layout/wrapper';
+import ErrorMsg from '@/components/common/error-msg';
+import { useGetProductQuery } from '@/redux/features/productApi';
+import ProductDetailsBreadcrumb from '@/components/breadcrumb/product-details-breadcrumb';
+import ProductDetailsArea from '@/components/product-details/product-details-area';
+import PrdDetailsLoader from '@/components/loader/prd-details-loader';
+
+const ProductDetailsPageWithVideo = () => {
+  const { data: product, isLoading, isError } = useGetProductQuery("641e887d05f9ee1717e13496");
+  // decide what to render
+  let content = null;
+  if (isLoading) {
+    content = <PrdDetailsLoader loading={isLoading}/>;
+  }
+  if (!isLoading && isError) {
+    content = <ErrorMsg msg="There was an error" />;
+  }
+  if (!isLoading && !isError && product) {
+    content = (
+      <>
+        <ProductDetailsBreadcrumb category={product.category.name} title={product.title} />
+        <ProductDetailsArea productItem={product} />
+      </>
+    );
+  }
+  return (
+    <Wrapper>
+      <SEO pageTitle="Product Details" />
+      <HeaderTwo style_2={true} />
+      {content}
+      <Footer primary_style={true} />
+    </Wrapper>
+  );
+};
+
+export default ProductDetailsPageWithVideo;
+
