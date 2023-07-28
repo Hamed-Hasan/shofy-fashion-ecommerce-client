3b9e69f37666f63b364fb42d30b96b6510f4a1a1
John Doe <hamedhasan422421@gmail.com>
2023-07-28 12:07:00 +0300

    - Create ArrowPrevTwo SVG component for displaying an icon representing a backward arrow. - The SVG includes two paths forming a simple arrow pointing left.

---
+++ src/svg/arrow-prev-2.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ArrowPrevTwo = () => {
+  return (
+    <svg
+      width="16"
+      height="14"
+      viewBox="0 0 16 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.00073 6.99989L15 6.99989"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M6.64648 1.5L1.00011 6.99954L6.64648 12.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default ArrowPrevTwo;
