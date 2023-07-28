2c9bb78335b62a12c9a7d86c4f94d687cd059310
John Doe <hamedhasan422421@gmail.com>
2023-07-28 13:15:06 +0300

    Add CloseTwo component
    Implemented the CloseTwo component, which displays an SVG icon representing a cross or close button. This component provides users with a visual element for closing or dismissing certain UI elements.

---
+++ src/svg/category-menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CategoryMenu = () => {
+  return (
+    <svg
+      width="18"
+      height="14"
+      viewBox="0 0 18 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 1C0 0.447715 0.447715 0 1 0H15C15.5523 0 16 0.447715 16 1C16 1.55228 15.5523 2 15 2H1C0.447715 2 0 1.55228 0 1ZM0 7C0 6.44772 0.447715 6 1 6H17C17.5523 6 18 6.44772 18 7C18 7.55228 17.5523 8 17 8H1C0.447715 8 0 7.55228 0 7ZM1 12C0.447715 12 0 12.4477 0 13C0 13.5523 0.447715 14 1 14H11C11.5523 14 12 13.5523 12 13C12 12.4477 11.5523 12 11 12H1Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default CategoryMenu;
---
+++ src/svg/close-2.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CloseTwo = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11 1L1 11"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 1L11 11"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CloseTwo;
