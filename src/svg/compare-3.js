8855283945ce9abf576afc7289976f3d7eb4da5b
John Doe <hamedhasan422421@gmail.com>
2023-07-28 13:19:50 +0300

    Introduce CompareThree component
    Added the CompareThree component, which displays an SVG icon representing a comparison or compare action with three elements. This component enhances the user interface by providing a visual element for indicating comparisons or contrasting items, specifically with three options, in the application.

---
+++ src/svg/compare-3.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const CompareThree = () => {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.4144 6.16828L14 3.58412L11.4144 1"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.48883 3.58374L14 3.58374"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.07446 8.32153L1.48884 10.9057L4.07446 13.4898"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 10.9058H1.48883"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CompareThree;
