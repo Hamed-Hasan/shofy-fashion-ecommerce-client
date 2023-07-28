468b65db5212f64a2c295f2f068f2ec00f5404e1
John Doe <hamedhasan422421@gmail.com>
2023-07-28 13:28:59 +0300

    Create Facebook component
    Added the Facebook component, which displays an SVG icon representing the Facebook logo. This component can be used to add a Facebook social media link or feature to the user interface.

---
+++ src/svg/facebook.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Facebook = () => {
+  return (
+    <svg
+      width="8"
+      height="15"
+      viewBox="0 0 8 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 0H5.81818C4.85376 0 3.92883 0.383116 3.24688 1.06507C2.56493 1.74702 2.18182 2.67194 2.18182 3.63636V5.81818H0V8.72727H2.18182V14.5455H5.09091V8.72727H7.27273L8 5.81818H5.09091V3.63636C5.09091 3.44348 5.16753 3.25849 5.30392 3.1221C5.44031 2.98571 5.6253 2.90909 5.81818 2.90909H8V0Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default Facebook;
