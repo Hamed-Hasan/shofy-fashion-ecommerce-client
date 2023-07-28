e4d7c65bd64675b725da8b6c4d9ccff9ebca5980
John Doe <hamedhasan422421@gmail.com>
2023-07-28 14:00:54 +0300

    Add ShapeLineSm component SVG icon

---
+++ src/svg/shape-line-sm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ShapeLineSm = () => {
+  return (
+    <svg
+      width="64"
+      height="20"
+      viewBox="0 0 64 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M62 13.1107C1.91792 -5.41202 -3.10865 1.22305 5.00242 18.3636"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeMiterlimit="3.8637"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default ShapeLineSm;
