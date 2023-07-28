0a774be122acd1b7ec92a3dce4f9b51f1097f5e6
John Doe <hamedhasan422421@gmail.com>
2023-07-28 14:45:36 +0300

    - Implement authSlice for managing authentication-related state - Add actions to handle user login and logout - Set and remove user information from the state and cookies

---
+++ src/redux/features/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
+
+const initialState = {
+  accessToken: undefined,
+  user: undefined,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    userLoggedIn: (state, { payload }) => {
+      state.accessToken = payload.accessToken;
+      state.user = payload.user;
+    },
+    userLoggedOut: (state) => {
+      state.accessToken = undefined;
+      state.user = undefined;
+      Cookies.remove('userInfo');
+    },
+  },
+});
+
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
+export default authSlice.reducer;
