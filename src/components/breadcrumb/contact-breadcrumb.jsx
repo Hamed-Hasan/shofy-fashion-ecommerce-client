3ee6752ce8d638339fa9519eaae9d20b527bf3e9
John Doe <hamedhasan422421@gmail.com>
2023-07-28 22:52:48 +0300

    Developed a new React component, ContactBreadcrumb, to display breadcrumbs specific to the Contact page. The component ensures the website's navigation is more intuitive and user-friendly. Integrated the ContactBreadcrumb into the existing website structure for consistent design and layout. Utilized React functional components for enhanced performance and maintainability. Verified the component's functionality to ensure it correctly displays the breadcrumb links. Conducted thorough testing to confirm responsiveness and compatibility across various devices.

---
+++ src/components/breadcrumb/contact-breadcrumb.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ContactBreadcrumb = () => {
+  return (
+    <section className="breadcrumb__area include-bg text-center pt-95 pb-50">
+      <div className="container">
+        <div className="row">
+          <div className="col-xxl-12">
+            <div className="breadcrumb__content p-relative z-index-1">
+              <h3 className="breadcrumb__title">Keep In Touch with Us</h3>
+              <div className="breadcrumb__list">
+                <span>
+                  <a href="#">Home</a>
+                </span>
+                <span>Contact</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactBreadcrumb;
