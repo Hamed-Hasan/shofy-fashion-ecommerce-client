f0e9b08d34c7e0fcabfab0d62801b9635de13e27
John Doe <hamedhasan422421@gmail.com>
2023-07-28 11:53:46 +0300

    - Implement InputRange component for displaying a range slider using 'react-range'. - Define 'STEP', 'MIN', and 'MAX' props to configure the slider's behavior. - Use 'values' prop to set the current values of the range slider. - Provide a 'handleChanges' callback prop to handle updates to the slider's values. - Render the range slider with custom track and thumb styles. - The track background changes color between

---
+++ src/ui/input-range/index.jsx
@@ -0,0 +1,48 @@
+import { getTrackBackground, Range } from "react-range";
+
+const InputRange = ({ STEP, MIN, MAX, values, handleChanges }) => {
+  return (
+    <>
+      <Range
+        step={STEP}
+        min={MIN}
+        max={MAX}
+        values={values}
+        onChange={(vals) => handleChanges(vals)}
+        renderTrack={({ props, children }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '3px',
+              width: '100%',
+              background: getTrackBackground({
+                values: values,
+                colors: ["#EDEDED", "#0989FF", "#EDEDED"],
+                min: MIN,
+                max: MAX
+              }),
+            }}
+          >
+            {children}
+          </div>
+        )}
+        renderThumb={({ props, isDragged }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '17px',
+              width: '5px',
+              backgroundColor: '#0989FF',
+              backgroundColor: isDragged ? "#0989FF" : "#0989FF"
+            }}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+
+export default InputRange;
