e5363ea39d9178796692cd00de75fac03ed9e548
John Doe <hamedhasan422421@gmail.com>
2023-07-28 14:07:03 +0300

    Add SliderPrevBtn component SVG icon

---
+++ src/svg/slider-prev-btn.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SliderPrevBtn = () => {
+  return (
+    <svg
+      width="8"
+      height="14"
+      viewBox="0 0 8 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 13L1 7L7 1"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SliderPrevBtn;
