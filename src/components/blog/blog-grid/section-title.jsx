70ec7fc73de5549dc0055fb28f75d320f4b582cc
John Doe <hamedhasan422421@gmail.com>
2023-07-28 23:23:54 +0300

    Added SectionTitle component for blog section headings Created a stylish title section with pre-title and main title

---
+++ src/components/blog/blog-grid/section-title.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SectionTitle = () => {
+  return (
+    <>
+      <section className="tp-section-title-area pt-95 pb-80">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-8">
+              <div className="tp-section-title-wrapper-7">
+                <span className="tp-section-title-pre-7">Blog Standard</span>
+                <h3 className="tp-section-title-7">Insight and advice <br /> From our Expert team.</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SectionTitle;
